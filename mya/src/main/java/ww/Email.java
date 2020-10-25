9d08df8d028910d66eb7d43a3140a31bddfec314
jiawei313 <55999005+jiawei313@users.noreply.github.com>
2020-10-24 21:37:46 -0500

    Add files via upload

---
+++ mya/src/main/java/ww/Email.java
@@ -0,0 +1,15 @@
+/**
+ * The email bean that will be used in the getUserEmail response.
+ */
+public class Email {
+
+  private String email;
+
+  public String getEmail() {
+    return this.email;
+  }
+
+  public void setEmail(String email) {
+    this.email = email;
+  }
+}
